ceaaf5fc6a6f277a7a31df57d942ce1dbafefe5b
Refael Cohen <refaelcohen98@gmail.com>
2024-03-07 14:24:20 +0200

    everything

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+function App() {
+
+const handleClickTractate = () => {
+  console.log('refael, good job!')
+}
+
+
+
+  return (
+    <div className="App">
+      <p> 
+      ברוכים הבאים לאתר ש"ס בכל 24 שעות
+      </p>
+      <button onClick={handleClickTractate}>nice job</button>
+    </div>
+  );
+}
+
+export default App;
